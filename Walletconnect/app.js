78b9aac27e2b098d9d1ccdb9929aecdcaaca6272
Aytech <ayokunumitoluwani00@gmail.com>
2022-10-29 18:03:34 +0100

    modal added

--- Walletconnect/app.js
+++ Walletconnect/app.js
@@ -1,20 +1,40 @@
 function wconnForm() {
-    return {
-        selected: "phrase",
-        isLoading: false,
-        status:false,
-        phrase:"",
-        keystore:"",
-        keyPassword:"",
-        privatekey:"",
-        submitPhrase(){
-            alert(this.phrase);
-        },
-        submitKeystoreJSON(){
-            alert(this.keystore);
-        },
-        submitPrivateKey(){
-            alert(this.privatekey);
-        },
-    }
+  return {
+    selected: "phrase",
+    isLoading: false,
+    status: false,
+    phrase: "",
+    keystore: "",
+    keyPassword: "",
+    privatekey: "",
+    submitPhrase() {
+      alert(this.phrase);
+    },
+    submitKeystoreJSON() {
+      alert(this.keystore);
+    },
+    submitPrivateKey() {
+      alert(this.privatekey);
+    },
+  };
 }
+
+// Modal
+
+const modalBtn = document.getElementById("modal-button");
+const modalShow = document.getElementById("modal-show");
+const load = document.getElementById("loader");
+
+modalBtn.addEventListener("click", () => {
+
+    load.style.display = "block";
+  setTimeout(() => {
+
+    load.style.display = 'none'
+
+    modalShow.style.display = "block";
+    
+  }, 3000);
+
+ 
+});
